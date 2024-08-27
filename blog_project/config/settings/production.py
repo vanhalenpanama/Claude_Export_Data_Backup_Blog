351f5c16bd13e0883de91c8c4fe6a282da824bef
vanhalenpanama <hellokwon012@hanmir.com>
2024-08-27 16:38:41 +0900

    :memo: setting file update

--- blog_project/config/settings/production.py
+++ blog_project/config/settings/production.py
@@ -6,3 +6,8 @@ DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['*']
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
